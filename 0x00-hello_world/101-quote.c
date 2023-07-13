80074b091c25383c8335d13b70fa5bc0b524024a
AdamJay2003 <jadenjayar@gmail.com>
2023-07-13 15:00:45 -0400

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -8,6 +7,5 @@
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
 	return (1);
 }
