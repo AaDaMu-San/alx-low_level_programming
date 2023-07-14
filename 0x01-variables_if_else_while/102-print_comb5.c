f36c8cc8c2274e9a4b9a9a26f150cd8f84a7c2ee
AdamJay2003 <jadenjayar@gmail.com>
2023-07-14 15:19:28 -0400

    12.experimwnt

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,18 +2,16 @@
 
 int main() {
     int i, j;
-    int count = 0;
 
     for (i = 0; i <= 99; i++) {
         for (j = i; j <= 99; j++) {
-            count++;
-            putchar('0' + (i / 10));
-            putchar('0' + (i % 10));
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
             putchar(' ');
-            putchar('0' + (j / 10));
-            putchar('0' + (j % 10));
+            putchar(j / 10 + '0');
+            putchar(j % 10 + '0');
 
-            if (count < 4950) {
+            if (!(i == 98 && j == 99)) {
                 putchar(',');
                 putchar(' ');
             }
