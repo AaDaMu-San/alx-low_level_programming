6a04b28bce701e7b61c625a5dab86145575a3b57
AdamJay2003 <jadenjayar@gmail.com>
2023-07-14 15:06:44 -0400

    12.experimwnt

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,22 +5,22 @@ int main() {
 
     for (i = 0; i <= 99; i++) {
         for (j = i; j <= 99; j++) {
-            // Print first number's tens digit
+
             putchar('0' + (i / 10));
 
-            // Print first number's ones digit
+
             putchar('0' + (i % 10));
 
-            // Print space separator
+
             putchar(' ');
 
-            // Print second number's tens digit
+
             putchar('0' + (j / 10));
 
-            // Print second number's ones digit
+
             putchar('0' + (j % 10));
 
-            // Print comma and space separator
+
             if (i != 99 || j != 99) {
                 putchar(',');
                 putchar(' ');
