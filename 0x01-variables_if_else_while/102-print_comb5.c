c60218fe021637d0e1e6fffe90e1c6e57a5239ee
AdamJay2003 <jadenjayar@gmail.com>
2023-07-14 15:20:46 -0400

    12.experimwnt

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int i, j;
-
-    for (i = 0; i <= 99; i++) {
-        for (j = i; j <= 99; j++) {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-            putchar(' ');
-            putchar(j / 10 + '0');
-            putchar(j % 10 + '0');
-
-            if (!(i == 98 && j == 99)) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
+  for (int i = 0; i <= 99; i++) {
+    for (int j = 0; j <= 99; j++) {
+      // Only print the combination if the two numbers are different.
+      if (i != j) {
+        putchar('0' + i / 10);
+        putchar('0' + i % 10);
+        putchar(' ');
+        putchar('0' + j / 10);
+        putchar('0' + j % 10);
+        putchar(',');
+        putchar(' ');
+      }
     }
-
-    return 0;
+  }
+  return 0;
 }
-
