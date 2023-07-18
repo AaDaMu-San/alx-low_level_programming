17e1e6b845d1f4db3af7ffbea0b17b5881ecc374
AdamJay2003 <jadenjayar@gmail.com>
2023-07-18 09:09:11 -0400

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0
  */
-void print_alphabet(void)
+/*void print_alphabet(void)
 {
 	int i;
 
@@ -12,7 +12,7 @@ void print_alphabet(void)
 	{
 		putchar(i);
 	}
-}
+}*/
 
 int main(void)
 {
