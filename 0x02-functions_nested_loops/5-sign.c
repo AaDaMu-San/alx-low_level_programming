d02ada52fe5c71938278ac0c5e6970fb210ee0b0
AdamJay2003 <jadenjayar@gmail.com>
2023-07-18 11:42:39 -0400

    5.1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,11 +8,7 @@
 
 int print_sign(int n)
 {
-	if (n == 0)
-	{
-		return (0);
-	}
-	else if (n > 0)
+	if (n > 0)
 	{
 		return (1);
 	}
@@ -20,4 +16,5 @@ int print_sign(int n)
 	{
 		return (-1);
 	}
+	return (0);
 }
