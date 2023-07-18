f644c887804d18100f421050e355909ef7c0d704
AdamJay2003 <jadenjayar@gmail.com>
2023-07-18 11:14:51 -0400

    x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,22 +2,18 @@
 /**
  * print_alphabet_x10 - Prints the alphabet 10x
  */
-void print_alphabet(void);
+
 void print_alphabet_x10(void)
 {
 	int i;
+	char c;
 
 	i = 0;
 	while (i < 10)
-	{
-		void print_alphabet(void)
+	{	
+		for (c = 'a'; c <= 'z'; c++)
 		{
-			char c;
-
-			for (c = 'a'; c <= 'z'; c++)
-			{
-				_putchar(c);
-			}
+			_putchar(c);
 		}
 		i++;
 	}
