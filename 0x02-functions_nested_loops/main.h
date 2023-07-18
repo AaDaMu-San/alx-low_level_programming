686198bf4b5462063a7ca2b7e45d942f42debe48
AdamJay2003 <jadenjayar@gmail.com>
2023-07-18 10:22:43 -0400

    fix main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,23 +1,2 @@
-/*
-#include <stdio.h>
-#include <unistd.h>
-*/
 int _putchar(char c);
 void print_alphabet(void);
-/*
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-void print_alphabet(void)
-{
-	char ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		_putchar(ch);
-		_putchar('\n');
-	}
-}
-*/
