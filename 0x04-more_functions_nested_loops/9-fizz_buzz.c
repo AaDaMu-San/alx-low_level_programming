657c2c46e4c07aa36f07d591d7b8987dac057453
AaDaMu-San <adamgayar2003@gmail.com>
2023-07-20 16:22:03 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entrypoint
+ *
+ * Return: 0 success
+ */
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+			putchar(' ');
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+			putchar(' ');
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			putchar(' ');
+		}
+	}
+}
