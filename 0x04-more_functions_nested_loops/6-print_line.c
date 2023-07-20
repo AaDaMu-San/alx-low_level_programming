864e5334452f65dca5e0d4f1f5bb1d443bb5f0a5
AaDaMu-San <adamgayar2003@gmail.com>
2023-07-20 15:27:30 -0700

    6.3

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,9 +7,16 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		putchar('_');
+		_putchar('\n');
 	}
-	putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
 }
