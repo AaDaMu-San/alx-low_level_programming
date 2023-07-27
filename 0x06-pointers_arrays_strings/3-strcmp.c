6d665d629b6ae531ffa88a7caefa70e604b33982
AaDaMu-San <adamgayar2003@gmail.com>
2023-07-27 16:53:09 -0700

    3.1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,21 +10,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	i = 0;
-	while (s1[i] != '\0')
+	while (*s1)
 	{
-		if (s2[i] == '\0')
-			return (1);
+		if (*s1 != *s2)
+			break;
 
-		else if (s1[i] < s2[i])
-			return (-1);
-
-		else if (s1[i] < s2[i])
-			return (1);
-
-		i++;
+		s1++;
+		s2++;
 	}
-	return (0);
+
+	return (s1 - s2);
 }
