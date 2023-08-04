0ef7a0903224a0e4461af7e2ee393f62ed48286f
AaDaMu-San <adamgayar2003@gmail.com>
2023-08-04 16:49:28 -0700

    0 fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 /**
  * main - entry point.
- * @argc: argc
- * @argv: argv
+ * @argc: argc.
+ * @argv: argv.
  *
  * Return: Always 1 success.
  */
-int main(int argc __attribute__((unused)), char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ * @argc: argc.
+ * @argv: argv.
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
