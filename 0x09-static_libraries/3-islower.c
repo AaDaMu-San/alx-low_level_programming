39bec25e8a111195ceefa8140969b38e56f5415f
AaDaMu-San <137951791+AaDaMu-San@users.noreply.github.com>
2023-08-05 01:41:06 +0300

    Update 3-islower.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,1 +1,15 @@
-
+#include "main.h"
+/**
+ * _islower - Entry
+ * @c: character to test.
+ *
+ * Return: 1 (true) else 0 (false)
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
