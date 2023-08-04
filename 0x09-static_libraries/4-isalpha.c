4b181f344d24751759b373bd866c4ac2537cf307
AaDaMu-San <137951791+AaDaMu-San@users.noreply.github.com>
2023-08-05 01:41:57 +0300

    Create 4-isalpha.c

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - Check is the character is an alphabet character
+ * @c: is the input to test.
+ *
+ * Return: 1 (true) else 0 (false)
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
