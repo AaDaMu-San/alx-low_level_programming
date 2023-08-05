d2c291f56d14facb9c2e32199519241e05559f54
AaDaMu-San <adamgayar2003@gmail.com>
2023-08-04 17:24:11 -0700

    updt

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point.
+ * @argc: argc.
+ * @argv: argv.
+ * Return: always 0.
+ */
+int main(int argc, char **argv)
+{
+	if (argc = 3)
+	{
+		printf("%d", atoi(argv[1]) * atoi(argv[2]));
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+	return (0);
+}
