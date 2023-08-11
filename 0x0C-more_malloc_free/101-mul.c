2ef49d078bb10e5834336b8803cefcd761ae792b
AaDaMu-San <adamgayar2003@gmail.com>
2023-08-10 18:30:03 -0700

    101

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - multiplies two positive numbers.
+ * @argc:
+ * @argv:
+ * Return: true 0, else 1.
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		exit(98);
+	}
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
