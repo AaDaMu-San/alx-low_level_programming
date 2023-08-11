9484ba88d0529858b35ab4bcc6dbf533b1487205
AaDaMu-San <adamgayar2003@gmail.com>
2023-08-10 18:09:28 -0700

    2 fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,24 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-/*
-char *_mems(char *s, char b, unsigned int n);
-
-**
- * _mems - allocates mem and fill it with constant bytes.
- * @s: mem area.
- * @b: char.
- * @n: no of time to cpy b.
- * Return: ptr to s.
- /
-char *_mems(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		s[i] = b;
-	return (s);
-}*/
-
 /**
  * *_calloc - function that allocates memory for an array, using malloc.
  * @nmemb: array.
