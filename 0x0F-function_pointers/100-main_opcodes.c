c1e888f2a2d39f4e8d896ae13abe4cd788f2194d
AdamJay2003 <jadenjayar@gmail.com>
2023-08-17 19:24:10 -0400

    4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int b, i;
+	int (*adres)(int, char **) = main;
+	unsigned char opcode;
+
+	if (argc != 2)
+	{
+		printf("1Error\n");
+		exit(1);
+	}
+
+	b = atoi(argv[1]);
+
+	if (b < 0)
+	{
+		printf("2Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < b; i++)
+	{
+		opcode = *(unsigned char *)adres;
+		printf("%.2x", opcode);
+
+		if (i == (b - 1))
+			continue;
+		printf(" ");
+		adres++;
+	}
+	printf("\n");
+	return (0);
+}
