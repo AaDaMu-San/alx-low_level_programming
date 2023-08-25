956c50bc480a13139861580a7e60c4a39a2bc973
AdamJay2003 <jadenjayar@gmail.com>
2023-08-24 22:26:00 -0400

    crated 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * b - print from init.
+ */
+void b(void) __attribute__((constructor));
+void b(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
