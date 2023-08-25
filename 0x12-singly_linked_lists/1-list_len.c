438638017ee6c1f4423891e1defc8b03c9cc1c5d
AdamJay2003 <jadenjayar@gmail.com>
2023-08-24 21:59:07 -0400

    crated 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 
 /**
- *
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: list.
+ * Return: num of elemnts.
  */
 size_t list_len(const list_t *h)
 {
