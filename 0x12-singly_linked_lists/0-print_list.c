043586239de98b09862c43e61b7d658f5e109ca1
AdamJay2003 <jadenjayar@gmail.com>
2023-08-24 21:54:19 -0400

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,12 +7,13 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
-	for (i = 0; h; i++)
+	while (h)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		printf("[%li] %s\n", strlen(h->str), h->str ? h->str : "(nil)");
 		h = h->next;
+		i++;
 	}
 	return (i);
 }
