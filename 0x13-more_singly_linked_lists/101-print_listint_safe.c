a65bc63360fa5ae50345235ddb5fde3644265983
AdamJay2003 <jadenjayar@gmail.com>
2023-08-31 20:20:15 -0400

    101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,19 +7,19 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t n = 0;
+	size_t c = 0;
 
 	while (head && head->next < head)
 	{
 		printf("[%p] %d\n", (void *) head, head->n);
-		n++;
+		c++;
 		head = head->next;
 	}
 	if (head)
 	{
 		printf("[%p] %d\n", (void *) head, head->n);
-		n++;
+		c++;
 		printf("-> [%p} %d\n", (void *) head->next, head->next->n);
 	}
-	return (n);
+	return (c);
 }
